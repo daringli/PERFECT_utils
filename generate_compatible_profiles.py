6f056756d2a521da949546aedd9678f2b3a699b6
daringli <bstefan@chalmers.se>
2015-12-18 14:48:17 +0100

    D

--- generate_compatible_profiles.py
+++ generate_compatible_profiles.py
@@ -213,7 +213,8 @@ def generate_compatible_profiles(simul,**kwargs):
     etaiHatPre =(lambda psiN: (niPed-niCoreGrad*(psiMinPed-psi[0]) +  niCoreGrad* (psiN-psi[0])))
     etaiHatPed =(lambda psiN: (niPed-niCoreGrad*(psiMinPed-psi[0]) +  niCoreGrad* (psiN-psi[0])))
     PhiTop=0.894
-    print "THat aft:" +str(THatAft[main_index](1))
+    for i in [0,1,2]:
+        print "THat aft "+str(i)+" : " +str(THatAft[i](1))
     print "nHat aft:" +str(niHatAft(1)*simul.nBar)
     etaiHatAft =(lambda psiN: niHatAft(psiN)*numpy.exp((PhiTop*Zs[main_index]/THatAft[main_index](psiN))*2*simul.omega/simul.Delta))
     etailist=[etaiHatPre,etaiHatPed,etaiHatAft]
