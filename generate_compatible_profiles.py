0e9158e391722df8a0e2e9f2566d72725f4cf4ec
daringli <bstefan@chalmers.se>
2015-12-18 14:12:39 +0100

    Changes index to the proper one in etaHat calculation.

--- generate_compatible_profiles.py
+++ generate_compatible_profiles.py
@@ -213,7 +213,7 @@ def generate_compatible_profiles(simul,**kwargs):
     etaiHatPre =(lambda psiN: (niPed-niCoreGrad*(psiMinPed-psi[0]) +  niCoreGrad* (psiN-psi[0])))
     etaiHatPed =(lambda psiN: (niPed-niCoreGrad*(psiMinPed-psi[0]) +  niCoreGrad* (psiN-psi[0])))
     CPhi=0.894
-    etaiHatAft =(lambda psiN: niHatAft(psiN)*numpy.exp((CPhi*Zs[main_index]/THatAft[species](psiN))*2*simul.omega/simul.Delta))
+    etaiHatAft =(lambda psiN: niHatAft(psiN)*numpy.exp((CPhi*Zs[main_index]/THatAft[main_index](psiN))*2*simul.omega/simul.Delta))
     etailist=[etaiHatPre,etaiHatPed,etaiHatAft]
     etaHats[main_index]=bezier_transition(etailist,psiList,pairList,psi)
     #nHats[mI] = interp1d(psi, nHats[mI], kind='cubic')
