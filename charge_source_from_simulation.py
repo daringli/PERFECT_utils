b7e3a9c30cd5d2470120432e322b16f68fd4bce9
bs <nope>
2016-10-01 20:53:16 +0200

    gg

--- charge_source_from_simulation.py
+++ charge_source_from_simulation.py
@@ -3,6 +3,8 @@ from perfect_simulations_from_dirs import perfect_simulations_from_dirs
 import sys
 
 dirlist=sys.argv[1:]
+
+normlist_filename,species_filename,psiN_to_psi = ("norms.namelist","species","psiAHat")
 simulList=perfect_simulations_from_dirs(dirlist,normlist_filename,species_filename,psiN_to_psi)
 simul = simulList[0]
 
